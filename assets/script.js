6413615b7a0998b04c996c4cd0aff0467ee76c7f
Sergio Rojas <sergrojas29@gmail.com>
2023-04-03 21:48:45 -0400

    Added more JS code ie functions and console output

--- assets/script.js
+++ assets/script.js
@@ -27,11 +27,48 @@ for (var x = 0;
     }
 
 */
-var topics = ["HTML","CSS","Git","JavaScript"]
-for (let x = 0; x < topics.length; x++) {
+var topics = ["HTML","CSS","Git","JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+/*for (let x = 0; x < topics.length; x++) {
     console.log("Let's Study " + topics[x]+ "!");
     if (x===3) {
         console.log("Please try agian!");  
     }
     
+}*/
+
+function listtopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+        
+    }
+    
 }
+
+function SelectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!")
+       
+     }
+    else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+        
+     }
+    else if (randomTopic === 'git') {
+        console.log("Let's study Git!");
+           
+    }
+    else if (randomTopic === 'JavaScript') {
+        console.log("let's Study JavaScript");
+        
+        
+    } else {
+        console.log('Please try again!');
+    }
+}
+console.log('Here are the topic we learned through Prework:');
+listtopics();
+console.log('Which topic should we study first?');
+SelectTopic();
+
